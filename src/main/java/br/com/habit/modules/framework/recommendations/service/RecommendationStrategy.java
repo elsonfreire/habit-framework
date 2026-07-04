6e93791157e2ab701e7e032aa53c32eace8125e2
Thiago Cordeiro <thiagoty52@gmail.com>
2026-07-04 11:50:44 -0300

    feat: change recommendation strategy to be an abstract class

--- src/main/java/br/com/habit/modules/framework/recommendations/service/RecommendationStrategy.java
+++ src/main/java/br/com/habit/modules/framework/recommendations/service/RecommendationStrategy.java
@@ -2,7 +2,15 @@ package br.com.habit.modules.framework.recommendations.service;
 
 import br.com.habit.modules.framework.user.model.User;
 
-public interface RecommendationStrategy {
+public abstract class RecommendationStrategy {
+    protected int scoreByLocation(User me, User other) {
+        if (me.getCity() != null && me.getCity().equalsIgnoreCase(other.getCity())) {
+            return 5;
+        } else if (me.getState() != null && me.getState().equalsIgnoreCase(other.getState())) {
+            return 3;
+        }
+        return 0;
+    }
     
-    int calculateScore(User me, User other);
+    public abstract int calculateScore(User me, User other);
 }
--- src/main/java/br/com/habit/modules/musicist/recommendations/MusicRecommendationStrategy.java
+++ src/main/java/br/com/habit/modules/musicist/recommendations/MusicRecommendationStrategy.java
@@ -7,17 +7,11 @@ import br.com.habit.modules.framework.user.model.User;
 import br.com.habit.modules.musicist.profile.MusicProfile;
 
 @Component
-public class MusicRecommendationStrategy implements RecommendationStrategy {
+public class MusicRecommendationStrategy extends RecommendationStrategy {
     
   @Override
   public int calculateScore(User me, User other) {
-    int score = 0;
-
-    if (me.getCity() != null && me.getCity().equalsIgnoreCase(other.getCity())) {
-      score += 5;
-    } else if (me.getState() != null && me.getState().equalsIgnoreCase(other.getState())) {
-      score += 3;
-    }
+    int score = scoreByLocation(me, other);
 
     MusicProfile musicProfile = (MusicProfile) me.getDomainProfile();
     MusicProfile otherMusicProfile = (MusicProfile) other.getDomainProfile();
