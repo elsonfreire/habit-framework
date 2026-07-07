9d8c8a63c3d05537a3b26b57291bca02cd54e30a
elsonfreire <elsonfreirefilho@gmail.com>
2026-07-07 00:36:48 -0300

    Fix repository trying to access interests

--- src/main/java/br/com/habit/modules/framework/practice/controller/PracticeController.java
+++ src/main/java/br/com/habit/modules/framework/practice/controller/PracticeController.java
@@ -22,7 +22,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/practices")
+@RequestMapping("/practices")
 @RequiredArgsConstructor
 public class PracticeController {
 
